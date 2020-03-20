052fb759bcc1a263c66a514383d4925f25a63f8e
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-20 10:07:43 +0100

    Make use of the single Glide instance provided by Koin for image loading

--- app/src/main/java/se/stylianosgakis/marsrealestate/overview/BindingAdapters.kt
+++ app/src/main/java/se/stylianosgakis/marsrealestate/overview/BindingAdapters.kt
@@ -5,22 +5,22 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.RequestManager
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 import se.stylianosgakis.marsrealestate.R
 import se.stylianosgakis.marsrealestate.model.MarsProperty
+
+object GlideInstance : KoinComponent {
+    val glide: RequestManager by inject()
+}
 
 @BindingAdapter("imageUrl")
 fun ImageView.bindImage(imageUrl: String?) {
     imageUrl?.let { url ->
         val parsedImageUrl = url.toUri().buildUpon().scheme("https").build()
-        Glide.with(this.context)
+        GlideInstance.glide
             .load(parsedImageUrl)
-            .apply(
-                RequestOptions()
-                    .placeholder(R.drawable.loading_animation)
-                    .error(R.drawable.ic_broken_image)
-            )
             .into(this)
     }
 }
