939a7ea345137990976fb6d00348d35308102946
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2020-03-22 01:14:29 +0100

    Fix bug with type not inferred properly on getProperties method

--- app/src/main/java/se/stylianosgakis/marsrealestate/repository/MarsRepository.kt
+++ app/src/main/java/se/stylianosgakis/marsrealestate/repository/MarsRepository.kt
@@ -9,12 +9,12 @@ class MarsRepository(
     private val api: MarsApiService
 ) {
     suspend fun getProperties(filter: String? = null): List<MarsProperty> {
-        return withContext(Dispatchers.IO) {
+        return withContext<List<MarsProperty>>(Dispatchers.IO) {
             try {
                 val response = api.getProperties(filter)
-                val properties = response.body()
-                if (response.isSuccessful && properties != null) {
-                    return@withContext properties
+                if (response.isSuccessful) {
+                    val properties = response.body()
+                    return@withContext properties ?: listOf()
                 }
             } catch (e: UnknownHostException) {
             }
